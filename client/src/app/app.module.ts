cc6c26a78a05a5c87b49446e474df034a9393b3b
sebasdawg <sebastien.dagenais@polymtl.ca>
2022-02-11 23:35:06 -0500

    material cards

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './components/app/app.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { MealSlotComponent } from './components/meal-slot/meal-slot.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,12 @@ import { MealSlotComponent } from './components/meal-slot/meal-slot.component';
     MainPageComponent,
     MealSlotComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
