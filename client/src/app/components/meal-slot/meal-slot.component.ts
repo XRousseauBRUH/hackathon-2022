476d61888bd88922b309b32b4a771dae99c93a79
Marc-Olivier <marc-olivier.lemieux@polymtl.ca>
2022-02-12 18:38:04 -0500

    fix add/delete button

--- client/src/app/components/meal-slot/meal-slot.component.ts
+++ client/src/app/components/meal-slot/meal-slot.component.ts
@@ -22,7 +22,6 @@ export class MealSlotComponent implements OnInit {
   receipe: String = null
   ingredients: String = null
   image: String = null
-  action: String = null;
   addMeal: boolean = null;
   showMenu: boolean = false;
   @Input() fullMeal: any;
@@ -31,15 +30,12 @@ export class MealSlotComponent implements OnInit {
   constructor(public dialog: MatDialog) {}
 
   public ngOnInit(): void {
-    console.log(this.fullMeal);
-    console.log(this.presentDay);
     console.log(this.fullMeal['name']);
     this.meal = this.fullMeal['name'];
     this.ingredients = this.fullMeal['ingredients'];
     this.receipe = this.fullMeal['receipe'];
     this.image = this.fullMeal['image'];
     this.type = this.fullMeal['type'];
-    this.action = 'add';
     this.addMeal = true;
   }
 
